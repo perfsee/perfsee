ef62f1a271e97ea7b9b33824a1b94046459b650c
jixiaomeng <jixiaomeng@bytedance.com>
2023-05-15 14:53:10 +0800

    chore: add package analyze

---
+++ packages/jsonr/src/__tests__/jsonr.bench.ts
@@ -0,0 +1,50 @@
+import { readFile } from 'fs/promises'
+import path from 'path'
+
+import Test from '@perfsee/package'
+
+import JSONR from '..'
+
+Test(`Parse with test.json`, [
+  {
+    test: async () => {
+      const file = await readFile(path.join(__dirname, './test_cases/test.json'), 'utf-8')
+      return () => JSON.parse(file)
+    },
+    options: {
+      name: `JSON.parse`,
+    },
+  },
+  {
+    test: async () => {
+      const file = await readFile(path.join(__dirname, './test_cases/test.json'), 'utf-8')
+      return () => JSONR.parse(file)
+    },
+    options: {
+      name: `JSONR.parse`,
+    },
+  },
+])
+
+Test(`Stringify with test.json`, [
+  {
+    test: async () => {
+      const file = await readFile(path.join(__dirname, './test_cases/test.json'), 'utf-8')
+      const obj = JSON.parse(file)
+      return () => JSON.stringify(obj)
+    },
+    options: {
+      name: `JSON.stringify`,
+    },
+  },
+  {
+    test: async () => {
+      const file = await readFile(path.join(__dirname, './test_cases/test.json'), 'utf-8')
+      const obj = JSONR.parse(file)
+      return () => JSONR.stringify(obj)
+    },
+    options: {
+      name: `JSONR.stringify`,
+    },
+  },
+])
