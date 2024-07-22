ce4769853afca2b4337d64d2fd89d9cfe6b946f4
lizimeow <465652891@qq.com>
2024-07-22 15:21:10 +0800

    feat(platform): pages layout

--- packages/platform/src/modules/project/settings/cards/index.tsx
+++ packages/platform/src/modules/project/settings/cards/index.tsx
@@ -1,10 +1,18 @@
+import { SelectionMode } from '@fluentui/react'
 import { ReactNode, useMemo } from 'react'
+
+import { Table, TableColumnProps } from '@perfsee/components'
 
 import { CardContainer, CardWrapper } from './styled'
 
 type SettingCardsProps<T> = {
   items: T[]
   onRenderCell: (item: T) => ReactNode
+}
+
+type SettingTableProps<T> = {
+  items: T[]
+  columns: TableColumnProps<T>[]
 }
 
 export const SettingCards = <T,>(props: SettingCardsProps<T>) => {
@@ -18,4 +26,10 @@ export const SettingCards = <T,>(props: SettingCardsProps<T>) => {
   }, [items, onRenderCell])
 
   return <CardContainer>{elements}</CardContainer>
+}
+
+export const SettingTable = <T,>(props: SettingTableProps<T>) => {
+  const { items, columns } = props
+
+  return <Table items={items} columns={columns} selectionMode={SelectionMode.none} />
 }
